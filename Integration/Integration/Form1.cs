20d012e1e410ac2ea7db459c478bdd2e204ae470
Yurii <jurasikmaniac@gmail.com>
2014-10-21 02:25:02 +0700

    calass integretor

--- Integration/Integration/Form1.cs
+++ Integration/Integration/Form1.cs
@@ -16,5 +16,15 @@ namespace Integration
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
