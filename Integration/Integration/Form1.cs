d1f0c86f744b3592dbf757bafe5979a6fb468fec
Yurii <jurasikmaniac@gmail.com>
2014-10-21 05:12:11 +0700

    parralel rect

--- Integration/Integration/Form1.cs
+++ Integration/Integration/Form1.cs
@@ -30,8 +30,10 @@ namespace Integration
 
         private void button1_Click(object sender, EventArgs e)
         {
-            textBox5.Text = Integrator.Tropez(textBox1.Text,Double.Parse(textBox2.Text),Double.Parse(textBox3.Text),Convert.ToInt32(textBox4.Text)).ToString();
-            textBox6.Text = Integrator.Rectangle(textBox1.Text, Double.Parse(textBox2.Text), Double.Parse(textBox3.Text), Convert.ToInt32(textBox4.Text)).ToString();
+            string s = Integrator.Rectangle(textBox1.Text, Double.Parse(textBox2.Text), Double.Parse(textBox3.Text), Convert.ToInt32(textBox4.Text)).ToString();
+            //textBox5.Text = Integrator.Tropez(textBox1.Text,Double.Parse(textBox2.Text),Double.Parse(textBox3.Text),Convert.ToInt32(textBox4.Text)).ToString();
+            //textBox6.Text = Integrator.Rectangle(textBox1.Text, Double.Parse(textBox2.Text), Double.Parse(textBox3.Text), Convert.ToInt32(textBox4.Text)).ToString();
+            textBox6.Text = "0";
         }
     }
 }
--- Integration/Integration/Integrator.cs
+++ Integration/Integration/Integrator.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Integration
@@ -41,11 +42,30 @@ namespace Integration
             {
                 result += Function(a + h * i - h / 2, func); //Вычисляем в средней точке и добавляем в сумму
             }
-            
+            //Parallel.For<double>(1, n-1, () => 0.0, (j, loop, subtotal) =>
+            //{
+            //    subtotal += Function(a + h * j - h / 2, func);
+            //    return subtotal;
+            //},
+            //(x) => Add(ref result, x)
+            //);
             
             result *= h;
 
             return result;
+        }
+
+        public static double Add(ref double location1, double value)
+        {
+            double newCurrentValue = 0;
+            while (true)
+            {
+                double currentValue = newCurrentValue;
+                double newValue = currentValue + value;
+                newCurrentValue = Interlocked.CompareExchange(ref location1, newValue, currentValue);
+                if (newCurrentValue == currentValue)
+                    return newValue;
+            }
         }
 
         public static double Function(double x, string func)
