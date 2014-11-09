16824bd0eb30e7f4c32dcc9a3eec4aefdc35fb78
Yurii <jurasikmaniac@gmail.com>
2014-11-09 17:46:21 +0600

    dsgd

--- Drawer/Drawer/Interpolator.cs
+++ Drawer/Drawer/Interpolator.cs
@@ -59,7 +59,7 @@ namespace Drawer
             if (points.Count == 0)
             {
                 return 0;
-            } 
+            }
             double res = points[0].Y, F, den;
             int i, j, k;
             for (i = 1; i < points.Count; i++)
@@ -82,12 +82,62 @@ namespace Drawer
             }
             return res;
         }
+
+        public static double NewtonFront(double x, PointPairList points, double step)
+        //для равноудаленных узлов
+        {
+            int n = points.Count;
+            if (n < 3) return 0D;
+            double[,] mas = new double[n + 2, n + 1];
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < n + 1; j++)
+                {
+                    if (i == 0 && j < n)
+                        mas[i, j] = points[j].X;
+                    else if (i == 1 && j < n)
+                        mas[i, j] = points[j].Y;
+                }
+            }
+            int m = n;
+            for (int i = 2; i < n + 2; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    mas[i, j] = mas[i - 1, j + 1] - mas[i - 1, j];
+                }
+                m--;
+            }
+
+            double[] dy0 = new double[n + 1];
+
+            for (int i = 0; i < n + 1; i++)
+            {
+                dy0[i] = mas[i + 1, 0];
+            }
+
+            double res = dy0[0];
+            double[] xn = new double[n];
+            xn[0] = x - mas[0, 0];
+
+            for (int i = 1; i < n; i++)
+            {
+                double ans = xn[i - 1] * (x - mas[0, i]);
+                xn[i] = ans;
+                ans = 0;
+            }
+
+            int m1 = n + 1;
+            int fact = 1;
+            for (int i = 1; i < m1; i++)
+            {
+                fact = fact * i;
+                res = res + (dy0[i] * xn[i - 1]) / (fact * Math.Pow(step, i));
+            }
+            return res;
+        }
+
+
+
     }
-
-
-
-
-
-
-
 }
